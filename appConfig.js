40ebdd5bd4c148afca29f808decca30adb61666e
gijslebesque <gijslebesque@gmail.com>
2020-10-03 00:42:46 +0200

    delete todo

--- appConfig.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  title: 'Gatsby Starter Event Calendar',
-  subTitle: 'The easiest option to share events!',
-  formLink: 'https://forms.gle/NUDSxEs1kB6Nd7Ty6',
-  limitMonthInTheFuture: 3,
-  theme: require('./themes/main.json'),
-};
--- src/utils/parseDate.ts
+++
@@ -1,5 +0,0 @@
-import { parse } from 'date-fns';
-
-const DATE_FORMAT = 'MM/dd/yyyy';
-
-export const parseEventDate = (date: string) => parse(date, DATE_FORMAT, 0);
