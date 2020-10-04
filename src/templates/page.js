79ff649e96ee92d72d2af98e916d13e476e51ceb
gijslebesque <gijslebesque@gmail.com>
2020-10-04 11:36:54 +0200

    fixed query for google sheet rows

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,18 +1,64 @@
-import React, { useState } from 'react';
+import React from 'react';
 import SEO from '../components/seo';
 import Layout from '../components/layout';
 import { normaliseDataGrid } from '../utils/normaliseData';
+import { useStaticQuery, graphql } from 'gatsby';
 
 import Calender from '../components/calendar';
 
-const Page = ({ pageContext: { node, calenderDate }, data }) => {
+const Page = ({ pageContext: { node } }) => {
+  const data = useStaticQuery(graphql`
+    query MyQuery {
+      allGoogleSheetFormResponses1Row {
+        edges {
+          node {
+            naamvandeshow
+            welkedatumwiltuboeken
+            status
+          }
+        }
+      }
+      allCosmicjsShows {
+        edges {
+          node {
+            slug
+            metadata {
+              show_name
+              show_image {
+                imgix_url
+              }
+            }
+          }
+        }
+      }
+    }
+  `);
+
   const showData = {
     ...node.metadata,
   };
 
-  console.log(node, calenderDate);
   const rows = normaliseDataGrid(data.allCosmicjsShows.edges);
-  const [showForm, setShowForm] = useState(false);
+  let calenderDate = { confirmed: [], pending: [], cancelled: [] };
+
+  data.allGoogleSheetFormResponses1Row.edges
+    .map((e) => ({
+      ...e.node,
+    }))
+    .filter((e) =>
+      e.naamvandeshow.toLowerCase().includes(node.title.toLowerCase())
+    )
+    .forEach((s) => {
+      const { status, welkedatumwiltuboeken } = s;
+      if (String(status) === 'bevestigd') {
+        calenderDate.confirmed.push(welkedatumwiltuboeken);
+      } else if (String(status) === 'geannuleerd') {
+        calenderDate.cancelled.push(welkedatumwiltuboeken);
+      } else {
+        calenderDate.pending.push(welkedatumwiltuboeken);
+      }
+    });
+
   return (
     <>
       <Layout rows={rows}>
@@ -23,14 +69,15 @@ const Page = ({ pageContext: { node, calenderDate }, data }) => {
             <Calender requests={calenderDate} to={node.to} from={node.from} />
           </div>
           <div className="position-relative">
-            <h1>{showData.show_name}</h1>
-            <h2 className="underline">{showData.theatre_name}</h2>
-            <button
-              className="btn btn-primary"
-              onClick={() => setShowForm(true)}
-            >
-              Boek nu
-            </button>
+            <div className="flex-between">
+              <div>
+                <h1>{showData.show_name}</h1>
+                <h2 className="underline">{showData.theatre_name}</h2>
+              </div>
+              <a href="#google-form" className="btn btn-primary">
+                Boek nu
+              </a>
+            </div>
             <div>
               <div
                 className="about"
@@ -47,9 +94,11 @@ const Page = ({ pageContext: { node, calenderDate }, data }) => {
           </div>
         </div>
 
-        <div>
-          <div className="flex-start flex-center">
+        <div className="margin-t-m">
+          <div className="flex-start flex-center margin-t-m">
             <iframe
+              title="google-form"
+              id="google-form"
               src="https://docs.google.com/forms/d/e/1FAIpQLScI1ED5-411gh5mkqk21XdqJtrSIHTOiWHUNC3UGm_bUiTiWQ/viewform?embedded=true"
               width="600"
               height="500"
@@ -65,23 +114,5 @@ const Page = ({ pageContext: { node, calenderDate }, data }) => {
     </>
   );
 };
-
-export const query = graphql`
-  query pageQueryForNav {
-    allCosmicjsShows {
-      edges {
-        node {
-          metadata {
-            show_name
-            show_image {
-              imgix_url
-            }
-          }
-          slug
-        }
-      }
-    }
-  }
-`;
 
 export default Page;
